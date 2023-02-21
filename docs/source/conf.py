786f60ad7bc5b0f648662127789a4c37c2c8a06e
Vincent Choqueuse <vincent.choqueuse@gmail.com>
2023-02-21 19:41:14 +0100

    new project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,12 +55,12 @@ exclude_patterns = []
 #
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
-html_logo = "logo.svg"
+html_logo = "_static/logo.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
 
 html_css_files = [
     'css/custom.css',
